a6371e44a03673e225dd16c847fc5eb313d91f16
Nate Clark <natec425@gmail.com>
2018-11-27 14:12:22 -0600

    Add assertResolvesTo helper

--- app/tests.py
+++ app/tests.py
@@ -1,24 +1,45 @@
-from django.test import TestCase
-from django.urls import reverse
+import django.test
+from django.urls import reverse, resolve
+from django.urls.exceptions import Resolver404
 from app.models import Link
+
+
+class TestCase(django.test.TestCase):
+    def assertResolvesTo(self, url, view_name, kwargs=None):
+        kwargs = kwargs or {}
+        try:
+            match = resolve(url)
+        except Resolver404:
+            raise AssertionError(
+                f'{url} didn\'t resolve to a view at all!\n\n'
+                f'You expected it to resolve to {view_name} with kwargs={kwargs}.\n\n'
+                'Have you added a path to your urlpatterns for it?\n'
+                'Are all of the necessary url parameters in place?') from None
+        else:
+            self.assertEqual(
+                match.view_name, view_name,
+                f'{url} match to {match.view_name} instead of {view_name}!')
+            self.assertEqual(
+                match.kwargs, kwargs,
+                f'{url} match with kwargs {match.kwargs} instead of {kwargs}')
 
 
 class TestCreateView(TestCase):
     def test_step1_root_resolves_to_app_create(self):
         'The path for root (the empty path) should resolve to a view named app:create'
-        response = self.client.get('/')
-        self.assertEqual(response.resolver_match.view_name, 'app:create')
+        self.assertResolvesTo('/', 'app:create')
 
     def test_step2_get_app_create_renders_app_create_html(self):
         'Making a GET request to app:create should render with app/create.html'
-        response = self.client.get(reverse('app:create'))
+        self.client.get(reverse('app:create'))
         self.assertTemplateUsed('app/create.html')
 
     def test_step3_post_app_create_with_valid_url_creates_link(self):
         'Make a POST request to app:create with a valid url should create a new Link'
-        response = self.client.post(
+        self.client.post(
             reverse('app:create'),
-            {'url': 'https://www.basecampcodingacademy.org'})
+            {'url': 'https://www.basecampcodingacademy.org'},
+        )
 
         self.assertTrue(
             Link.objects.filter(
@@ -42,7 +63,9 @@ class TestCreateView(TestCase):
     def test_step5_post_app_create_with_invalid_url_response_with_422(self):
         'POSTing to app:create with an invalid url should respond with UNPROCESSABLE_ENTITY 422'
         response = self.client.post(
-            reverse('app:create'), {'url': 'not a valid url'})
+            reverse('app:create'),
+            {'url': 'not a valid url'},
+        )
 
         self.assertEqual(response.status_code, 422)
 
@@ -61,10 +84,7 @@ class TestShowView(TestCase):
     def test_step1_link_shortcode_resolves_to_app_show(self):
         '''The path for /link/1/ should resolve to app:show
         with an argument `short_code` of 1.'''
-        response = self.client.get('/link/1/')
-
-        self.assertEqual(response.resolver_match.view_name, 'app:show')
-        self.assertEqual(response.resolver_match.kwargs['short_code'], '1')
+        self.assertResolvesTo('/link/1/', 'app:show', kwargs={'short_code': 2})
 
     def test_step2_get_existing_link_renders_app_show_with_link(self):
         '''app:show with the short_code for an existing link
